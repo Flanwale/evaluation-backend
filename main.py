431153a4c593e009f0e7f1d24decdc8247a18f81
mz <1774527542@qq.com>
2026-01-19 20:39:43 +0800

    feat: add backend dockerfile and update main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ prisma = Prisma()
 # 允许跨域
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
